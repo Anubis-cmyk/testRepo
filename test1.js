02b1d00065d0175613264f8c2cc571116013ad92
Anubis-cmyk <it19099132@my.sliit.lk>
2021-11-01 14:15:17 +0530

    add console log to feature2

--- test1.js
+++ test1.js
@@ -1,7 +1,7 @@
 function feature1(){
-    console.log("feacture2.1 console log");
     //feactur1 function
 }
 function feature2(){
+    console.log("feacture2.1 console log");
     //feature2() function
 }
